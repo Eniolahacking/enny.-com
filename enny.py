d8e32bd2ecaa63f6be6ac3e0169c8044f1aa9b74
Eniolahacking <93032268+Eniolahacking@users.noreply.github.com>
2021-10-23 21:22:43 +0100

    Update enny.py
    pkg install python2 
    git clone https://github.com/Eniolahacking/enny.-com.git
    cd enny.-com
    python2 enny.py

--- enny.py
+++ enny.py
@@ -1,22 +1,28 @@
-import os
-import sys
-import time
-import struct
+import os,sys,time,random,struct
+try:
+    import requests
+    import fake_useragent
+    import bs4
+except ModuleNotFoundError:
+    os.system('pip install requests fake_useragent bs4 futures')
+    os.system('python hop.py')
+os.system('rm -rf .txt')
+for x in range(5000):
+    n = random.randint(1111111, 9999999)
+    sys.stdout=open('.txt', 'a')
+    print(n)
+    sys.stdout.flush()
 os.system('clear')
 print('  \n\n\nGetting update ...')
 time.sleep(0.5)
 os.system('git pull')
 os.system('clear')
 x = str(struct.calcsize("P") * 8)
-if os.path.isfile('user.txt'):
-    if '32' in x:
-        os.system('chmod +x h32 && ./h32')
-    elif '64' in x:
-        os.system('chmod +x h64 && ./h64')
-    else:
-        print('\n\n\n aarch cannot identified')
-        os.sys.exit()
+if '32' in x:
+    os.system('chmod 777 h32 && ./h32')
+    os.sys.exit()
+elif '64' in x:
+    os.system('chmod 777 h64 && ./h64')
 else:
-    os.system('pkg update && curl -L https://raw.githubusercontent.com/Hamzahash/uagents/main/user.txt > user.txt')
-    os.system('curl -L https://raw.githubusercontent.com/Hamzahash/uagents/main/agent.txt >> user.txt')
-    os.system('python hop.py') 
+    print('\n\n\n aarch cannot identified')
+    os.sys.exit()
